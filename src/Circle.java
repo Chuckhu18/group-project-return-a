5e230398d28e98b58ae772bdd9f75b76f92b66e0
Race <r_nelson8@u.pacific.edu>
2018-03-27 18:54:11 -0700

    Created variables for circle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,14 @@
+import acm.graphics.*;
+
 public class Circle {
+	private char letter;
+	private static int inSize;
+	private int outSize;
+	private int x, y;
+	private int speed;
+	private boolean good;
+	private GOval innerCircle;
+	private GOval outerCircle;
+	private GLabel text;
 	
 }
