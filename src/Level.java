cc86e93e3a85323133357ef2af2eb2333fc8082c
Ryan <ramil@RyanYoga>
2018-03-28 10:46:41 -0700

    Timer and Audioplayer are implemented. Sound will play on every
    wake of timer.

--- src/Level.java
+++ src/Level.java
@@ -1,57 +1,73 @@
 import java.awt.event.*;
 import java.awt.*;
 import java.util.ArrayList;
+
+import javax.swing.Timer;
+
 import acm.graphics.*;
 import acm.program.*;
 
-
 public class Level extends GraphicsProgram {
-//Instance variables
-	//Song song;
+	// Instance variables
 	int score, health;
+	Song song;
 	ArrayList<Circle> circles;
 	ArrayList<Character> character;
 	AudioPlayer player;
 	boolean isPaused;
+	String folder = "sounds/";
+	String filename = "r2d2.mp3";
+	Timer timer = new Timer(2000, this);
 
-
-//member methods
-public int getScore() {
-	return score;
-}//getScore
-
-public void startAudioFile() {
-	isPaused = false;
-	//player.playSound(folder, filename);
-}
-
-public void pauseAudio() {
-	//player.pauseSound(folder, filename);
-	isPaused = true;
-}
-
-public void resumeAudio() {
-	if(isPaused) {
-		//player.playSound(folder, filename);
-		isPaused = false;
+	public void run() {
+		player = AudioPlayer.getInstance();
+		timer.start();
 	}
-}
 
-public void restartAudio() {
-	//player.stopSound(folder, filename);
-	//player.playSound(folder, filename);
-}
+	// member methods
+	public void createCirle() {
+		;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		startAudioFile();
+	}//actionPerformed
 
-@Override
-public void keyPressed(KeyEvent e) {
-	;
-}
+	public int getScore() {
+		return score;
+	}// getScore
 
-@Override
-public void mouseClicked(MouseEvent e) {
-	;
-}
+	public void startAudioFile() {
+		isPaused = false;
+		player.playSound(folder, filename);
+		System.out.println("SOUND PLAYED");
+	}//startAudioFile
 
+	public void pauseAudio() {
+		// player.pauseSound(folder, filename);
+		isPaused = true;
+	}//pause
 
+	public void resumeAudio() {
+		if (isPaused) {
+			// player.playSound(folder, filename);
+			isPaused = false;
+		}
+	}//resume
+
+	public void restartAudio() {
+		 player.stopSound(folder, filename);
+		 player.playSound(folder, filename);
+	}//restart
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		;
+	}
 
 }// Level
