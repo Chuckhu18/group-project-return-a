c5bba6ba80ce00a305471b3d6d371d07e4c3ac65
Chuckhu18 <chuckhu301@yahoo.com>
2018-03-28 23:25:59 -0700

    Finished basic structure for Player classed

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
+import acm.graphics.GOval;
+import acm.graphics.GRect;
 
 public class Player {
+	private String name;
+	private ArrayList<String> score;
+	private String fileName;
+	Song song;
 	
 	public Player(){
+		
+	}
+	
+	public String getName() {
+		return "";
+	}
+	
+	public ArrayList getScore(){
+		score = new ArrayList<String>();
+		return score;
+	}
+	
+	public void saveScoreToFile() {
+		
+	}
+	
+	public void addScore(Song song, int scoreAdd) {
 		
 	}
 	
