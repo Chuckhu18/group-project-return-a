b7314282a463236c4e2ff1989d90d53745810e64
Ryan <r_miller17@u.pacific.edu>
2018-03-30 15:18:43 -0700

    Testing git push preferences

--- src/Level.java
+++ src/Level.java
@@ -7,7 +7,7 @@ import acm.program.*;
 
 public class Level extends GraphicsProgram {
 	// ***Instance variables***
-	int score, health, counter1=0;
+	int score, health, counter1 = 0;
 	Song song;
 	Circle circle;
 	ArrayList<Circle> circles;
@@ -19,29 +19,29 @@ public class Level extends GraphicsProgram {
 	Timer timer = new Timer(500, this);
 
 	public void createCircle() {
-		// 
-		if(characters.size()>0)
+		//
+		if (characters.size() > 0)
 			circles.add(new Circle(characters.remove(0), 10.0, 20.0, 20.0, 1.0, true));
 		else
-			circles.add(new Circle('7', 10.0, 20.0, 20.0, 1.0, false));
-		
+			circles.add(new Circle('7', song.getCirlceSize(), song.getShrinkSpeed(), 20.0, 1.0, false));
+
 	}
 
 	public void run() {
 		song = new Song(filename, 10, 1.0, 5, "sdafhjfdakfdsaf");
 		circles = new ArrayList<Circle>(); // Initializes ArrayList of Circles
 		characters = new ArrayList<Character>(); // Initializes ArrayList of characters
-		
+
 		// Adds characters to the characters ArrayList
 		// TODO: generate characters ArrayList from incoming Song data
 		characters.add('a');
 		characters.add('b');
 		characters.add('c');
-		
+
 		// Adds circle to the board using hardcoded values
-		// TODO: randomize start position, get size and speed data from incoming Song data
+		// TODO: randomize start position, get size and speed data from incoming Song
+		// data
 		createCircle();
-		
 		player = AudioPlayer.getInstance();
 		startAudioFile();
 		timer.start();
@@ -53,27 +53,20 @@ public class Level extends GraphicsProgram {
 	 */
 	public void actionPerformed(ActionEvent e) {
 		counter1++;
-		
-		if(counter1 % 6 == 0) {
+
+		if (counter1 % 6 == 0) {
 			createCircle(); // Make a new circle every few ticks to test
 		}
-		
-		/*
-		 *  TODO: Development note (Race, 03/29/18):
-		 *  I am thinking we might have been wrong to decide to have each circle with its own timer,
-		 *  I think in hindsight it's better to just call the shrink() function from this timer every
-		 *  time. Will talk about in class tomorrow and probably implement over the weekend.
-		 */
-		
-		if(circles.size()>=1) {
-			int count=0;
-			for(Circle circle:circles) {
+
+		if (circles.size() >= 1) {
+			int count = 0;
+			for (Circle circle : circles) {
 				circle.shrink();
-				if(circle.getOutSize() <= 0) {
+				if (circle.getOutSize() < 0) {
 					circles.remove(circle);
-				}else {			
-				System.out.println(count+": "+circle);
-				count++;
+				} else {
+					System.out.println(count + ": " + circle);
+					count++;
 				}
 			}
 			System.out.println();
