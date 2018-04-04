81e44d5300cd551867de3c0c71fa1ac739667abd
Ryan <r_miller17@u.pacific.edu>
2018-04-03 18:17:26 -0700

    fixed reader leak, changed "miss" color

--- src/Circle.java
+++ src/Circle.java
@@ -15,15 +15,21 @@ public class Circle {
 	private GOval outerCircle;
 	private GLabel text;
 
-
 	/**
 	 * Circle constructor
-	 * @param letter Letter that will be in the circle
-	 * @param outSize How much bigger the outer circle is than the inner circle
-	 * @param x - X coordinate of circle's location
-	 * @param y - Y coordinate of circle's location
-	 * @param speed - How fast the circle shrinks
-	 * @param good - If the circle gives points or not
+	 * 
+	 * @param letter
+	 *            Letter that will be in the circle
+	 * @param outSize
+	 *            How much bigger the outer circle is than the inner circle
+	 * @param x
+	 *            - X coordinate of circle's location
+	 * @param y
+	 *            - Y coordinate of circle's location
+	 * @param speed
+	 *            - How fast the circle shrinks
+	 * @param good
+	 *            - If the circle gives points or not
 	 */
 	public Circle(char letter, double outSize, double x, double y, double speed, boolean good) {
 		this.letter = letter;
@@ -32,16 +38,17 @@ public class Circle {
 		this.y = y;
 		this.speed = speed;
 		this.good = good;
-		
-		innerCircle = new GOval(x-(inSize/2),y-(inSize/2),inSize,inSize);
-		outerCircle = new GOval(innerCircle.getX()-(outSize/2),innerCircle.getY()-(outSize/2),inSize+outSize,inSize+outSize);
-		
+
+		innerCircle = new GOval(x - (inSize / 2), y - (inSize / 2), inSize, inSize);
+		outerCircle = new GOval(innerCircle.getX() - (outSize / 2), innerCircle.getY() - (outSize / 2),
+				inSize + outSize, inSize + outSize);
+
 		// TODO: calculate center of circle to correctly center the text
 		// Update 04/01: This is better but not ideal, will continue to experiment
-		text = new GLabel(Character.toString(letter),x+inSize/10,y+inSize/10);
-		
-		//Makes good circles blue and bad circles red
-		if(good) {
+		text = new GLabel(Character.toString(letter), x + inSize / 10, y + inSize / 10);
+
+		// Makes good circles blue and bad circles red
+		if (good) {
 			innerCircle.setColor(Color.BLUE);
 			outerCircle.setColor(Color.BLUE);
 			text.setColor(Color.BLUE);
@@ -50,13 +57,12 @@ public class Circle {
 			outerCircle.setColor(Color.RED);
 			text.setColor(Color.RED);
 		}
-			
+
 	}
-	
-	
+
 	/**
-	 * Full constructor - will never be used in practice
-	 * TODO: remove before final game is done, not sure if it might be needed to test later
+	 * Full constructor - will never be used in practice TODO: remove before final
+	 * game is done, not sure if it might be needed to test later
 	 */
 	public Circle(char letter, int outSize, int x, int y, int speed, boolean good, GOval innerCircle, GOval outerCircle,
 			GLabel text) {
@@ -70,78 +76,87 @@ public class Circle {
 		this.outerCircle = outerCircle;
 		this.text = text;
 	}
-	
-	
+
 	/**
-	 * Shrinks the circle by the amount specified by the speed variable in this class.
-	 * Also updates the outer GOval so it reflects this change
+	 * Shrinks the circle by the amount specified by the speed variable in this
+	 * class. Also updates the outer GOval so it reflects this change
 	 */
-	public void shrink(){
+	public void shrink() {
 		// TODO: makes circle shrink
 		outSize -= speed;
-		
+
 		// Does math to keep the outer circle centered
 		double newX = innerCircle.getX() - outSize / 2;
 		double newY = innerCircle.getY() - outSize / 2;
-				
+
 		outerCircle.setLocation(newX, newY);
-		outerCircle.setSize(inSize+outSize, inSize+outSize);
-		
+		outerCircle.setSize(inSize + outSize, inSize + outSize);
+
 	}
-	
+
 	/**
 	 * For testing purposes display the contents of this circle as a string
+	 * 
 	 * @return
 	 */
 	public String toString() {
 		String toReturn = "";
-		
-		toReturn += "Letter: "+letter+", Size (in,out): ("+inSize+","+df.format(new Double(inSize+outSize))+"), ";
-		toReturn += "X/Y Location: ("+df.format(x)+","+df.format(y)+"), Speed: "+speed+", Good: "+good;
-		
+
+		toReturn += "Letter: " + letter + ", Size (in,out): (" + inSize + "," + df.format(new Double(inSize + outSize))
+				+ "), ";
+		toReturn += "X/Y Location: (" + df.format(x) + "," + df.format(y) + "), Speed: " + speed + ", Good: " + good;
+
 		return toReturn;
 	}
+
 	public void removeCircles() {
 		innerCircle.setVisible(false);
 		outerCircle.setVisible(false);
 	}
-	
+
 	public void removeLabel() {
 		text.setVisible(false);
 		text = null;
 		innerCircle = null;
 		outerCircle = null;
 	}
-	
+
 	public char getLetter() {
 		return letter;
 	}
+
 	public static double getInSize() {
 		return inSize;
 	}
+
 	public double getOutSize() {
 		return outSize;
 	}
+
 	public double getX() {
 		return x;
 	}
+
 	public double getY() {
 		return y;
 	}
+
 	public GOval getInnerCircle() {
 		return innerCircle;
 	}
+
 	public GOval getOuterCircle() {
 		return outerCircle;
-	}	
+	}
+
 	public GLabel getLabel() {
 		return text;
 	}
+
 	public int getRemoveCounter() {
 		return removeCounter;
 	}
-	
-	
+
 	public void setRemoveCounter(int removeCounter) {
 		this.removeCounter = removeCounter;
 	}
--- src/Song.java
+++ src/Song.java
@@ -7,23 +7,29 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Song {
-// TODO: see how you can read from file the songs and hardcode it into this class
-	//look at the system design report on how to implement something like that
-	//make a constructor for song 
+	// TODO: see how you can read from file the songs and hardcode it into this
+	// class
+	// look at the system design report on how to implement something like that
+	// make a constructor for song
 	private String songName;
 	private double circleSize;
 	private double shrinkSpeed;
 	private int tempo;
 	private String circleList;
-	
-	
+	private String[] stringArray;
+
 	/***
 	 * 
-	 * @param s Song Name
-	 * @param cS Circle Size
-	 * @param sS Shrink Speed
-	 * @param temp Tempo
-	 * @param cL String of characters
+	 * @param s
+	 *            Song Name
+	 * @param cS
+	 *            Circle Size
+	 * @param sS
+	 *            Shrink Speed
+	 * @param temp
+	 *            Tempo
+	 * @param cL
+	 *            String of characters
 	 */
 	public Song(String s, double cS, double sS, int temp, String cL) {
 		songName = s;
@@ -32,6 +38,7 @@ public class Song {
 		tempo = temp;
 		circleList = cL;
 	}
+
 	public Song() {
 		songName = "";
 		circleSize = 0.0;
@@ -39,73 +46,77 @@ public class Song {
 		tempo = 0;
 		circleList = "";
 	}
-	
+
 	public String getSongName() {
 		return songName;
 	}
+
 	public double getCircleSize() {
 		return circleSize;
 	}
+
 	public String getCircleList() {
 		return circleList;
 	}
+
 	public double getShrinkSpeed() {
 		return shrinkSpeed;
 	}
+
 	public int getTempo() {
 		return tempo;
 	}
+
 	public void run() {
-		
+
 	}
-	//make this function outside of the class
+
+	// make this function outside of the class
 	public void readSong(String fileName) throws IOException {
-		
-		//File file = new File("src/Song.txt");
-		//for (String fileNames : file.list()) System.out.println(fileNames);
-	    FileReader file = new FileReader("src/"+fileName);
-	    BufferedReader reader = new BufferedReader(file);
-	    String line = reader.readLine();
-	    //System.out.println(line);
-	    while (line != null) {
-	    	SavingValues(line);
-	       // System.out.println(line);
-	        line = reader.readLine(); // what this does is updates the line
-	    }
-	   // System.out.println("hi");
-	   
-	
-	
-	//implement read from file method 
-	//to test this write a function to have all the variables printed out to the console
-}
-	
-	//create a constuctor for this partciular manner of input
-	// read from file outsdie of the class and create the song object within this class using the function below
-	//TODO make saving values a constructor
-	private void SavingValues(String lines) {
-		String[] stringArray = lines.split(";");
-		songName = stringArray[0];
-		double result = Double.parseDouble(stringArray[1]);			
-		circleSize = result;
-		double newSS = Double.parseDouble(stringArray[1]);
-		shrinkSpeed = newSS;
-		int newTempo = Integer.parseInt(stringArray[3]);
-		tempo = newTempo;
-		circleList = stringArray[4];
-		
+
+		// File file = new File("src/Song.txt");
+		// for (String fileNames : file.list()) System.out.println(fileNames);
+		FileReader file = new FileReader("src/" + fileName);
+		BufferedReader reader = new BufferedReader(file);
+		String line = reader.readLine();
+		// System.out.println(line);
+		SavingValues(line);
+		// System.out.println(line);
+
+		// System.out.println("hi");
+		reader.close();
+
+		// implement read from file method
+		// to test this write a function to have all the variables printed out to the
+		// console
 	}
-	
-	public static void main(String [ ] args)
-	{
-		Song sing = new Song(); //using default constructor
+
+	// create a constuctor for this partciular manner of input
+	// read from file outsdie of the class and create the song object within this
+	// class using the function below
+	// TODO make saving values a constructor
+	private void SavingValues(String lines) {
+		stringArray = lines.split(";");
+		songName = stringArray[0];
+		circleSize = Double.parseDouble(stringArray[1]);
+		shrinkSpeed = Double.parseDouble(stringArray[1]);
+		tempo = Integer.parseInt(stringArray[3]);
+		circleList = stringArray[4];
+
+		for (String string : stringArray) {
+
+			System.out.println(string);
+		}
+	}
+
+	public static void main(String[] args) {
+		Song sing = new Song(); // using default constructor
 		try {
 			sing.readSong("Song.txt");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.exit(0);
-		}  
-		
+		}
 	}
 }
