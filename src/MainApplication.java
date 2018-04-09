eb70da23705a36b0693f16e2c3ba49c4287dd6a2
Chuckhu18 <chuckhu301@yahoo.com>
2018-04-09 11:47:48 -0700

    committed enable to pull

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -4,7 +4,7 @@ import javax.swing.Timer;
 
 public class MainApplication extends GraphicsApplication {
 	public static final int WINDOW_WIDTH = 800;
-	public static final int WINDOW_HEIGHT = 600;
+	public static final int WINDOW_HEIGHT = 480;
 	public static final String MUSIC_FOLDER = "sounds";
 	private static final String[] SOUND_FILES = { "r2d2.mp3", "somethinlikethis.mp3" };
 
