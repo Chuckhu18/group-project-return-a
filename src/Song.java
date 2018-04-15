bc4389232ecc76a8eb2bedf48e849933b5e9ad53
Race <r_nelson8@u.pacific.edu>
2018-04-14 22:10:21 -0700

    Changed way songs are read from file to allow for more modularity in design

--- src/Song.java
+++ src/Song.java
@@ -5,18 +5,19 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Song {
 	// TODO: see how you can read from file the songs and hardcode it into this
 	// class
 	// look at the system design report on how to implement something like that
 	// make a constructor for song
-	private String songName;
-	private double circleSize;
-	private double shrinkSpeed;
-	private int tempo;
-	private String circleList;
-	private String[] stringArray;
+	private String songName; // Name of song
+	private double circleSize; // Starting size of the outside circles
+	private double shrinkSpeed; // How fast the circles shrink
+	private int tempo; // How often circles are created
+	private String circleList; // list of characters that will appear on the screen
+	private ArrayList<Integer> tempoChanges =  new ArrayList<Integer>(); // timestamps when we should change tempo
 
 	/***
 	 * 
@@ -66,26 +67,55 @@ public class Song {
 	public int getTempo() {
 		return tempo;
 	}
-
-	public void run() {
-
+	
+	private ArrayList<Integer> getTempoChanges() {
+		return tempoChanges;
+	}
+	
+	public Song(String filename) {
+		try {
+			readSong("songs/"+filename+".txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 
 	// make this function outside of the class
-	public void readSong(String fileName) throws IOException {		
-		//File file = new File("src/Song.txt");
-		//for (String fileNames : file.list()) System.out.println(fileNames);
-	    FileReader file = new FileReader("src/"+fileName);
+	private void readSong(String fileName) throws IOException {		
+	    FileReader file = new FileReader(fileName);
 	    BufferedReader reader = new BufferedReader(file);
+	    
+	    // Read in guarenteed aspects of a song
+	    songName = reader.readLine();
+	    circleSize = Double.parseDouble(reader.readLine());
+	    shrinkSpeed = Double.parseDouble(reader.readLine());
+	    tempo = Integer.parseInt(reader.readLine());
+	    circleList = reader.readLine();
+	    
+	    // Read in parts that might not be in every song
 	    String line = reader.readLine();
-	    //System.out.println(line);
-	    while (line != null) {
-	    	SavingValues(line);
-	       // System.out.println(line);
-	        line = reader.readLine(); // what this does is updates the line
+	    if(line!=null) {
+	    	String[] list = line.split(";");
+	    	for(int i = 0; i < list.length; i++) {
+	    		tempoChanges.add(Integer.parseInt(list[i]));
+	    		//System.out.println(tempoChanges.get(i));
+	    	}
+	    	
 	    }
-	   // System.out.println("hi");
+
 	   reader.close();
+	   
+	   // Print values entered for testing
+	   
+	   System.out.println(songName);
+	   System.out.println(circleSize);
+	   System.out.println(shrinkSpeed);
+	   System.out.println(tempo);
+	   System.out.println(circleList);
+	   for(int i = 0; i < tempoChanges.size(); i++)
+		   System.out.print(tempoChanges.get(i)+" ");
 	
 	
 	//implement read from file method 
@@ -95,24 +125,11 @@ public class Song {
 	//create a constuctor for this partciular manner of input
 	// read from file outsdie of the class and create the song object within this class using the function below
 	
-
-	// create a constuctor for this partciular manner of input
-	// read from file outsdie of the class and create the song object within this
-	// class using the function below
-	// TODO make saving values a constructor
-	private void SavingValues(String lines) {
-		stringArray = lines.split(";");
-		songName = stringArray[0];
-		circleSize = Double.parseDouble(stringArray[1]);
-		shrinkSpeed = Double.parseDouble(stringArray[2]);
-		tempo = Integer.parseInt(stringArray[3]);
-		circleList = stringArray[4];
-	}
 		
 	public static void main(String[] args) {
 		Song sing = new Song(); // using default constructor
 		try {
-			sing.readSong("Song.txt");
+			sing.readSong("src/songs/Song.txt");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
